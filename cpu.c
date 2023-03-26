e6fc1bb5e06666af15b50a9ee9e7955eff87f5dc
Emily Lim <emily.lim@wsu.edu>
2023-03-25 23:08:20 -0700

    Created pthreads and data structures

--- cpu.c
+++ cpu.c
@@ -1,6 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+
+// ./cpu -alg FCFS -input input.txt
+// ./cpu -alg RR -quantum 10 -input input.txt
+// ./cpu -alg RR -quantum 30 -input input.txt
+
+#define LINE_LEN 256
+
+pthread_mutex_t zero = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t one = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t two = PTHREAD_MUTEX_INITIALIZER;
+
+// doubly linked list data structure
+struct Node {
+  int data;
+  int priority;
+  int burst_number;
+  char * cpu_IO;
+ 
+  struct Node* next;
+  struct Node* prev;
+};
+
+struct Node* ready_head;
+struct Node* ready_tail;
+
+struct Node* io_head;
+struct Node* io_head;
+
+struct Node* dq;
+
+static void en_q(Node* head, Node* tail, int data, int burst){
+  struct Node* new_node = (struct Node*)malloc(sizeof());
+  new_node->data = data;
+  new_node->burst = burst;
+  new_data->next = NULL;
+  new_data->prev = NULL;
+  
+  if (head == NULL){
+    head = new_node;
+    tail = new_node;
+  }
+  else{
+    tail->next = new_node;
+    new_node-> = tail;
+    tail = new_node;
+  } 
+}
+
+static void de_q(Node* head, Node* tail){
+  if (head != NULL){
+  	dq = head;
+    head = head->next;
+    head->prev = NULL;
+    #free(dq)
+}
+
+static void sort_q(Node* head, Node* tail){
+  return 0;
+}
+  
+static void *read_file(void *input_file){
+  printf("inside read file\n");
+  FILE *fp = fopen((char *)input_file, "r");
+  // check if opening file was a success
+  if (fp == NULL) {
+    printf("Cannot open file\n");
+    pthread_exit(NULL);
+  } else {
+    printf("File successfully opened\n");
+  }
+  
+  char line[LINE_LEN];
+  
+  while (fgets(line, LINE_LEN, fp) != NULL){
+    printf("%s", line);
+//     if the thread encounters sleep, then thread sleeps before continuing to read file
+		if (line[0] == "sleep") {
+      printf("need to sleep");
+    }
+  }
+    
+  fclose(fp);
+  pthread_exit(NULL);
+}
+
 
 int main(int argc, char *argv[]){
 
@@ -9,6 +95,7 @@ int main(int argc, char *argv[]){
 
   char *sched_algo;
   sched_algo = argv[2];
+  
 
   // check for 6 args for RR
   if (strcmp(sched_algo, "RR") == 0){
@@ -28,32 +115,43 @@ int main(int argc, char *argv[]){
   }
   
   if (strcmp(argv[1], "-alg") == 0){
-    FILE *fp = fopen(input_file, "r");
-    // check if opening file was a success
-    if (fp == NULL) {
-      printf("Cannot open file\n");
-      exit(1);
-    }  
-  }
-
-  if (strcmp(sched_algo, "FCFS") == 0){
-    return 0;
-  }
-
-  if (strcmp(sched_algo, "SJF") == 0){
-    return 0;
-  }
-
-  if (strcmp(sched_algo, "PR") == 0){
-    return 0;
-  }
-
-  if (strcmp(sched_algo, "RR") == 0){
-    if (strcmp(argv[4], "0") == 0){
-			printf("Error: The quantum value give is zero");
-      exit(1);
+    printf("i am running\n");
+    pthread_t thread_input;
+    
+    pthread_create(&thread_input, NULL, read_file, (void *)input_file);
+    pthread_join(thread_input, NULL);
+    
+    if (strcmp(sched_algo, "FCFS") == 0){
+      return 0;
     }
-    return 0;
+
+    if (strcmp(sched_algo, "SJF") == 0){
+      return 0;
+    }
+
+    if (strcmp(sched_algo, "PR") == 0){
+      return 0;
+    }
+
+    if (strcmp(sched_algo, "RR") == 0){
+      if (strcmp(argv[4], "0") == 0){
+        printf("Error: The quantum value give is zero");
+        exit(1);
+      }
+      return 0;
+    }
   }
+  
+  // printf("Input File Name                  : %s\n", input_file)
+  // printf("CPU Scheduling Alg               : %s", sched_algo)
+  // if algorithm is RR, also print out the quantum
+  // if (strcmp(sched_algo, "RR") == 0) {
+  //		printf(" (%s)\n", quantum)
+	// } else {
+	//		printf("\n")
+	// }
+  // printf("Throughput                       : ")
+  // printf("Avg. Turnaround Time             : ")
+  // printf("Avg. Waiting Time in Ready Queue : ")
   
 }
